a4f5c3c48ed3c58393f84ac864b1f1caad82f0a5
Raylan Araujo <raylanra@gmail.com>
2024-09-12 14:41:55 -0300

    criado o ex009

---
+++ aula 11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'Brasil'
+
+console.log(`Vivendo em: ${país}`)
+if(país =='Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é extrangeiro')
+}
+
+//Nesse código temos o país que a pessoa está vivendo e a condição composta onde tem dois comandos. O primeiro comando que será execultado quando a condição (país =='Brasil') for verdadeira. E um segundo comando que será execultado quando a condição for falsa.
