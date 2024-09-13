c2296d457b164c15d6c2fd6538bd71553e67cdaa
Raylan Araujo <raylanra@gmail.com>
2024-09-13 15:14:28 -0300

    finalizado a primeira aula de exercicios - tudo funcionando perfeitamente

--- aula 12/ex014/script.js
+++ aula 12/ex014/script.js
@@ -1,1 +1,19 @@
-alert('ólá')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >=0 && hora < 12){
+        img.src = 'imagens/manha.png'
+        window.document.body.style.background = '#ffcf79'
+    } else if (hora >=12 && hora <19){
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#c7c4cb'
+    } else {
+        img.src = 'imagens/noite.png'
+        window.document.body.style.background = '#2c3f39'
+    }
+
+}
