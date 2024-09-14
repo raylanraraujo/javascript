f755b98f58e0cfa4b5050dbb2c4a4414b79fd726
Raylan Araujo <raylanra@gmail.com>
2024-09-14 09:03:53 -0300

    aula 13 criada

---
+++ aula 13/ambiente.js
@@ -0,0 +1,28 @@
+//Repeti/ção com teste no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+
+/* 
+//Repetição com teste no início
+
+var c = 1
+
+while(c <=6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
