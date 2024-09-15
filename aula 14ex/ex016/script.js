4bb0ffc87afbf0133f9c2d0d0659f5b8e9c7a20e
Raylan Araujo <raylanra@gmail.com>
2024-09-15 12:36:27 -0300

    criado as variáveis e a soma de teste funcionand

--- aula 14ex/ex016/script.js
+++ aula 14ex/ex016/script.js
@@ -1,9 +1,16 @@
 function contar() {
-    var ini = window.document.getElementById('txtini')
-    var fim = window.document.getElementById('txtfim')
-    var pas = window.document.getElementById('txtpas')
+    //criei a variável de cada elemento dos inputs
+    var ini = window.document.querySelector('input#txtini')
+    var fim = window.document.querySelector('input#txtfim')
+    var pas = window.document.querySelector('input#txtpas')
     var cont = ''
 
-  
+    //converti os dados dos inputs em números para os cálculos
+    var nini = Number(ini.value)
+    var nfim = Number(fim.value)
+    var npas = Number(pas.value)
+
+    cont = nini + npas
+    
     res.innerHTML = `A contagem inciando em ${ini.value} com um intervalo de ${pas.value} e terminando em ${fim.value} é: <br> ${cont}`
 }
