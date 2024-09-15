b9c6f8d332727d1fa7a8c5d396f98ecffc3de029
Raylan Araujo <raylanra@gmail.com>
2024-09-15 17:21:47 -0300

    contador funcionando

--- aula 14ex/ex016/script.js
+++ aula 14ex/ex016/script.js
@@ -1,21 +1,23 @@
 function contar() {
-    //criei a variável de cada elemento dos inputs
-    var ini = window.document.querySelector('input#txtini')
-    var fim = window.document.querySelector('input#txtfim')
-    var pas = window.document.querySelector('input#txtpas')
-    var res = window.document.querySelector('div#res')
+  //criei a variável de cada elemento dos inputs
+  var ini = window.document.querySelector('input#txtini')
+  var fim = window.document.querySelector('input#txtfim')
+  var pas = window.document.querySelector('input#txtpas')
+  var res = window.document.querySelector('div#res')
 
-    //converti os dados dos inputs em números para os cálculos
-    var nini = Number(ini.value)
-    var nfim = Number(fim.value)
-    var npas = Number(pas.value)
-    res.innerHTML = '';
+  //converti os dados dos inputs em números para os cálculos
+  var nini = Number(ini.value)
+  var nfim = Number(fim.value)
+  var npas = Number(pas.value)
+  res.innerHTML = '';
 
-  
-    
+  if (ini.value.length == 0) {
+    res.innerHTML = 'Impossível contar. Insira um número para iniciar!'
+  } else{
     for (let c = nini; c <= nfim; c += npas) {
-        res.innerHTML += `${c} &#9755;`;
-      }
+      res.innerHTML += `${c} &#9755;`;
+    }
+  }
 
-    res.innerHTML = `A contagem inciando em ${ini.value} com um intervalo de ${pas.value} e terminando em ${fim.value} é: <br> ${cont}`
-}
+ 
+} 
