e3a4fe8b3a5905eace630c9a365ac6db7ce47562
Raylan Araujo <raylanra@gmail.com>
2024-09-15 12:10:55 -0300

    ajeitado o idioma

--- aula 14ex/ex016/script.js
+++ aula 14ex/ex016/script.js
@@ -1,1 +1,9 @@
-document.querySelector('div#res')
+function contar() {
+    var ini = window.document.getElementById('txtini')
+    var fim = window.document.getElementById('txtfim')
+    var pas = window.document.getElementById('txtpas')
+    var cont = ''
+
+  
+    res.innerHTML = `A contagem inciando em ${ini.value} com um intervalo de ${pas.value} e terminando em ${fim.value} é: <br> ${cont}`
+}
