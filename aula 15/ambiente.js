510527b3b41f2bc1c10f48edab285ed9d36862ce
Raylan Araujo <raylanra@gmail.com>
2024-09-16 15:54:05 -0300

    criado os exercícios aula 15

---
+++ aula 15/ambiente.js
@@ -0,0 +1,13 @@
+let num = []
+console.log(`Nosso vetor num é o ${num}.`)
+console.log(num)
+
+let nume = [ 5 , 8 , 2 , 9 , 3]
+console.log(nume)
+console.log(`Nosso vetor nume é ${nume}`)
+console.log(`O vetor tem ${nume.length} posições`)
+console.log(`Mostre o índice da posição 3 --> ${nume[3]}`)
+console.log(`Em ordem crescente dos elementos nós temos ${nume.sort()}`)
+nume.push(1)
+console.log(nume)
+console.log (`O elemento 1 foi adicionado ao vetor após ele ter sido sido organizado em ordem crescente. Por isso quando ele é consultado vemos o elemento 1 no último índice [${nume}]`)
