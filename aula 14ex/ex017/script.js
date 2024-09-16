c2d26d60a086ae7e0eea0995878f66c93bf7c338
Raylan Araujo <raylanra@gmail.com>
2024-09-16 08:52:18 -0300

    feito o exercício 17

--- aula 14ex/ex017/script.js
+++ aula 14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('ólá')
+function gerar(){
+    var num = document.querySelector('input#inum')
+    var res = document.querySelector('div#res')
+    var n = Number(num.value)
+    res.innerHTML = ''
+
+    if (inum.value.length == 0){
+        res.innerHTML = `<p>[ERRO] <br> Por favor insira um número! </p>`
+
+    } else {
+        for (c=1; c<=10; c++) {
+        var r = c*n
+        res.innerHTML += `<p>${n} x ${c} = ${r}</p>` 
+        }
+    }
+   
+    
+} 
