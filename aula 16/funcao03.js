c6c46e74f56357149f94c2b41717eff0a220a708
Raylan Araujo <raylanra@gmail.com>
2024-09-17 14:49:10 -0300

    criado a funçao03

---
+++ aula 16/funcao03.js
@@ -0,0 +1,9 @@
+// a variavel V vai receber um parâmetro e quer escrever o dobro dele na tela.
+let v = function(x) {
+    return x*2
+}
+
+//passei para a minha variável um parâmetro 5 e ele conseguiu executar uma função.
+console.log(v(5))
+
+
