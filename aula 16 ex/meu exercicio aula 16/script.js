1a30fa233c601c542a3f3b57d3d2239a6b9e4b60
Raylan Araujo <raylanra@gmail.com>
2024-09-18 07:19:28 -0300

    iniciado o exercecicio

--- aula 16 ex/meu exercicio aula 16/script.js
+++ aula 16 ex/meu exercicio aula 16/script.js
@@ -1,7 +1,17 @@
 let numero = document.getElementById('inum')
-let 
+let res = document.getElementById('finalizar')
 
+numero = 2
 
 function adicionar() {
-    
+    let conjunto = []
+    conjunto.push(numero.value)
+    if (numero.value < 0 || numero.value > 100){
+        finalizar.innerHTML = '[Erro] Número inválido!'
+    }else {
+
+        finalizar.innerHTML += `Valor ${numero.value} foi adicionado. <p>`
+       
+    }
+    console.log(conjunto)
 }
